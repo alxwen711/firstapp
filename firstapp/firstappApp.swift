757eeac88ec39a247d46f215e38f32fbde701229
alxwen711 <alxwen711@gmail.com>
2023-06-17 14:31:48 -0700

    Initial Commit

---
+++ firstapp/firstappApp.swift
@@ -0,0 +1,17 @@
+//
+//  firstappApp.swift
+//  firstapp
+//
+//  Created by Alexander Wen on 2023-06-17.
+//
+
+import SwiftUI
+
+@main
+struct firstappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
